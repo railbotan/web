2f0296f71257baf24d48848519174c233656cfd7
Tripletri <Tripletri@yandex.ru>
2021-12-25 17:00:16 +0500

    1, 2 задание

---
+++ app.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+from flask import Response
+import sqlite3
+import random
+import io
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def cv_index():
+    cvs = get_cv()
+    res = ""
+    for i, cv in enumerate(cvs):
+        res += f"<h1>{i + 1})</h1>"
+        res += f"<p>Желаемая зарплата: {cv['salary']}.</p>"
+        res += f"<p>Образование: {cv['educationType']}.</p>"
+
+    return res
+
+
+@app.route("/dashboard")
+def dashboard():
+    cvs = get_cv()
+    return render_template('d3.html',
+                           cvs=cvs,
+                           graph_data=get_year_distribution(cvs))
+
+
+def dict_factory(cursor, row):
+    # обертка для преобразования
+    # полученной строки. (взята из документации)
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def get_cv():
+    con = sqlite3.connect('works.sqlite')
+    con.row_factory = dict_factory
+    res = list(con.execute('select * from works limit 20'))
+    con.close()
+    return res
+
+
+def get_year_distribution(data: list[dict]) -> dict[int, int]:
+    res = {}
+    for sign in data:
+        year = datetime.strptime(sign['dateModify'], "%Y-%m-%d").year
+        res[year] = 1 if year not in res else res[year] + 1
+    return res
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
