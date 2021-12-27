2f82da64a40961f1d6bf7008a7bff41007bb5ae8
Dmitry Ishoev <26188825+NetVoobrazhenia@users.noreply.github.com>
2021-12-27 09:26:13 +0500

    1 task

---
+++ d4.py
@@ -0,0 +1,75 @@
+import io
+import random
+import sqlite3
+
+from flask import Flask
+from flask import Response
+from flask import render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def cv_index():
+    cvs = get_cv()
+    res = ""
+    for i, cv in enumerate(cvs):
+        res += f"<h1>{i + 1})</h1>"
+        res += f"<p>Желаемая зарплата: {cv['salary']}.</p>"
+        res += f"<p>Образование: {cv['educationType']}.</p>"
+
+    return res
+
+
+def get_year(data):
+    res = {}
+    for e in data:
+        year = e['dateModify'][:4]
+        res[year] = 1 if year not in res else res[year] + 1
+    return res
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('d4.html',
+                           cvs=get_cv(),
+                           graph_data=get_year(get_cv()))
+
+
+def dict_factory(cursor, row):
+    # обертка для преобразования
+    # полученной строки. (взята из документации)
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def get_cv():
+    con = sqlite3.connect('works.sqlite')
+    con.row_factory = dict_factory
+    res = list(con.execute('select * from works limit 20'))
+    con.close()
+    return res
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
+
+app.run()
